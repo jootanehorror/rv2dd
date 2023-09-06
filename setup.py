a114a6216c652de65a685f5b7ada0b894ceca399
Oleg Kufa <103970725+jootanehorror@users.noreply.github.com>
2023-09-06 11:50:03 +0300

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+    name="rv2dd",
+    version="1.0.0",
+    author="Oleg Shishkin",
+    author_email="os.schischkin@gmail.com",
+    description="bivariate discrete distribution based on scipy",
+
+    long_description_content_type="text/markdown",
+    url="https://github.com/jootanehorror/rv2dd",
+    packages=["rv2dd"],
+    classifiers=[
+        "Programming Language :: Python :: 3.8",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+    ],
+)
